7d3371fb5e91c0e2724eff7d2fa81c00cf480085
mjk-limon <jhmasterlimon11@gmail.com>
2025-10-11 13:14:18 +0600

    submodule: ecom init

--- creative/ecom/assets/js/main.js
+++ creative/ecom/assets/js/main.js
@@ -23,10 +23,10 @@
 
         if (window.pageYOffset > sticky) {
             header_navbar.classList.add("sticky");
-            logo.src = 'assets/images/logo/logo-2.svg';
+            logo.src = 'assets/images/logo/logo.png';
         } else {
             header_navbar.classList.remove("sticky");
-            logo.src = 'assets/images/logo/logo.svg';
+            logo.src = 'assets/images/logo/logo.png';
         }
 
         // show or hide the back-top-top button
